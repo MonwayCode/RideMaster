08120caf8244b50c0f4b15e34e9586b08b79db78
MonwayCode <monika.tafelska@interia.pl>
2024-08-01 11:20:06 +0200

    Better Login page, fixed node moduls

--- frontend/src/login_register_validation/Login.js
+++ frontend/src/login_register_validation/Login.js
@@ -1,51 +1,51 @@
-
 import React from 'react';
+import logo from '../assets/logo.png';
 
 const Login = () => {
     return (
         <section className="vh-100" style={{ backgroundColor: '#D0B8A8' }}>
-            <div className="container py-5 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                    <div className="col col-xl-10">
+            <div className="container h-100">
+                <div className="row justify-content-center align-items-center h-100">
+                    <div className="col-lg-8 col-md-10 col-sm-12">
                         <div className="card" style={{ borderRadius: '1rem' }}>
                             <div className="row g-0">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                <div className="col-md-6 d-flex justify-content-center align-items-center"
+                                    style = {{ backgroundColor: '#F8EDE3', borderRadius: '1rem 0 0 1rem' }}>
                                     <img
-                                        src="./assets/logo.png"
-                                        alt="login form"
+                                        src={logo}
+                                        alt="logo"
                                         className="img-fluid"
-                                        style={{ borderRadius: '1rem 0 0 1rem' }}
+                                        style={{ borderRadius: '1rem 0 0 1rem', maxWidth: '100%', height: 'auto' }}
                                     />
                                 </div>
-                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-black">
+                                <div className="col-md-6 d-flex align-items-center ">
+                                    <div className="card-body p-8 text-black">
                                         <form>
-                                            <div className="d-flex align-items-center mb-3 pb-1">
-                                                <i className="fas fa-cubes fa-2x me-3" style={{ color: '#ff6219' }}></i>
-                                                <span className="h1 fw-bold mb-0">Logo</span>
-                                            </div>
-                                            <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: '1px' }}>
-                                                Sign into your account
-                                            </h5>
-                                            <div className="form-outline mb-4">
-                                                <input type="email" id="form2Example17" className="form-control form-control-lg" />
-                                                <label className="form-label" htmlFor="form2Example17">Email address</label>
+                                            <div className="d-flex align-items-center pb-1">
+                                                <i style={{ color: '#ff6219' }}></i>
+                                                <span className="h1 fw-bold mb-0">Login</span>
                                             </div>
                                             <div className="form-outline mb-4">
-                                                <input type="password" id="form2Example27" className="form-control form-control-lg" />
-                                                <label className="form-label" htmlFor="form2Example27">Password</label>
+                                                <input type="email" id="email" className="form-control form-control-lg" />
+                                                <label className="form-label" htmlFor="email">Email address</label>
+                                            </div>
+                                            <div className="form-outline mb-4">
+                                                <input type="password" id="password" className="form-control form-control-lg" />
+                                                <label className="form-label" htmlFor="password">Password</label>
                                             </div>
                                             <div className="pt-1 mb-4">
-                                                <button className="btn btn-dark btn-lg btn-block" type="button">
+                                                <button className="btn btn-lg btn-block" type="button" style={{backgroundColor: '#4E4562', color: '#FFECDE'}}>
                                                     Login
                                                 </button>
                                             </div>
-                                            <a className="small text-muted" href="#!">Forgot password?</a>
                                             <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>
-                                                Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a>
+                                                <div>
+                                                    Don't have an account?
+                                                </div>
+                                                <div>
+                                                    <a href="" style={{ color: '#393f81' }}>Register here</a>
+                                                </div>
                                             </p>
-                                            <a href="#!" className="small text-muted">Terms of use.</a>
-                                            <a href="#!" className="small text-muted">Privacy policy</a>
                                         </form>
                                     </div>
                                 </div>

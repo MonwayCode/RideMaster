7d64715781ec4ec5d1f1db25c274b03c7a9b5ec6
MonwayCode <monika.tafelska@interia.pl>
2024-10-14 17:07:50 +0200

    Login Validation

--- frontend/src/login_register_validation/Login.js
+++ frontend/src/login_register_validation/Login.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const Login = () => {
+import Validation from './LoginValid';
+
+function Login ()
+{
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const handleInput = (event) => {
+        const { name, value } = event.target;
+        setValues(prev => ({ ...prev, [name]: value }));
+        setErrors(prev => ({ ...prev, [name]: Validation({ ...values, [name]: value })[name] }));
+    };
+
+    const navigate = useNavigate();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(Validation(values));
+        if(errors.email === "" && errors.password === "")
+        {
+            axios.post('http://localhost:3001/logowanie', values).then(res => {
+                if(res.data.error === null)
+                {
+                    //setUserId(res.data.userId);
+                    window.localStorage.setItem("userId",res.data.userId);
+                    navigate('/');
+                }
+                else
+                {
+                    alert("Użytkownik nie istnieje")
+                }
+            }).catch(err => console.log(err));
+        }
+    };
+
     return (
         <section className="vh-100" style={{ backgroundColor: '#D0B8A8' }}>
             <div className="container h-100">
@@ -20,21 +59,23 @@ const Login = () => {
                                 </div>
                                 <div className="col-md-6 d-flex align-items-center ">
                                     <div className="card-body p-8 text-black">
-                                        <form>
+                                        <form onSubmit={handleSubmit}>
                                             <div className="d-flex align-items-center pb-1">
                                                 <i style={{ color: '#ff6219' }}></i>
                                                 <span className="h1 fw-bold mb-0">Logowanie</span>
                                             </div>
                                             <div className="form-outline mb-4">
-                                                <input type="email" id="email" className="form-control form-control-lg" />
+                                                <input type="email" name="email" id="email" className="form-control form-control-lg" onChange={handleInput} />
                                                 <label className="form-label" htmlFor="email">Adres email</label>
+                                                {errors.email && <div className='text-danger'>{errors.email}</div>}
                                             </div>
                                             <div className="form-outline mb-4">
-                                                <input type="password" id="password" className="form-control form-control-lg" />
+                                                <input type="password" id="password" name="password" className="form-control form-control-lg" onChange={handleInput} />
                                                 <label className="form-label" htmlFor="password">Hasło</label>
+                                                {errors.password && <div className='text-danger'>{errors.password}</div>}
                                             </div>
                                             <div className="pt-1 mb-4">
-                                                <button className="btn btn-lg btn-block" type="button" style={{backgroundColor: '#4E4562', color: '#FFECDE'}}>
+                                                <button className="btn btn-lg btn-block" type="submit" style={{backgroundColor: '#4E4562', color: '#FFECDE'}}>
                                                     Zaloguj
                                                 </button>
                                             </div>
---
+++ frontend/src/login_register_validation/LoginValid.js
@@ -0,0 +1,21 @@
+function Validation(values)
+{
+    let error = {};
+
+    if (values.email.trim() === "") {
+        error.email = "Pole nie może być puste!";
+    } else if (!values.email.includes("@")) {
+        error.email = "Podany tekst nie jest adresem e-mail";
+    } else {
+        error.email = "";
+    }
+
+    if (values.password.trim() === "") {
+        error.password = "Pole nie może być puste!";
+    } else {
+        error.password = "";
+    }
+
+    return error;
+}
+export default Validation;
